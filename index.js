61cd1cb2a6662e25892c4e9dfe346b1f7aab2cff
dlwjddms <jeongeun3.24lee@gmail.com>
2021-04-13 10:41:20 -0700

    forst step forst taste of typescript

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("heelp:");
+/* type script :: typed language
+ you need to tell the language what kind of varaible
+ and what kind of data is this variable
+ -> we can predict what is going to happen..
+ : compile can help you what is going om wrong
+*/
+/* normal js
+ but in ts sayHi(name,age).. this will get error ..
+ in js this doesn'y make error
+ */
+const name = "JJeong", age = 24, gender = "Female";
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name}, you are ${age}, you are ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
 console.log("heelp:")
+ /* type script :: typed language 
+  you need to tell the language what kind of varaible 
+  and what kind of data is this variable 
+  -> we can predict what is going to happen.. 
+  : compile can help you what is going om wrong
+*/
+
+/* normal js 
+ but in ts sayHi(name,age).. this will get error .. 
+ in js this doesn'y make error 
+ */
+const name = "JJeong",
+age = 24,
+gender = "Female"
+
+const sayHi = (name, age, gender?) => { // if you attach ? it is optional..!
+    console.log(`hello ${name}, you are ${age}, you are ${gender}`);
+}
+
+sayHi(name,age);
+
+/* This file si going to be a module 
+* is we don't do this ...we cannot make variable*/
+export {};
