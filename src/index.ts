7296a83010c074d64456bcb85b2201a57e2a2ec4
dlwjddms <jeongeun3.24lee@gmail.com>
2021-04-14 14:28:52 -0700

    class for js

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
-// type script will check that person has \tje same imterface with HUman 
-// you can make your own object with this 
-interface Human {
-  name : string,
-  gender : string,
-  age : number
+// interafce didn;t go to JS code  
+// sometime you wantto put interface in the JS -> we will use class instead of interface
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  // method that call everytime a object 
+  constructor(name :string, age:number, gender:string){ // if you do gender?:string gender will be optional
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
 // What if I want to pass an object, 
 //we have to check wether the pbject type is right
-const person = {
-  name : "JJeong",
-  age : 24,
-  gender : "Female"}
+
+const lynn = new Human("lynn",18,"F")
 
 const sayHi = (person : Human): void => { // void <- return typr
-    // (name, age, gender?) => if you attach ? it is optional..!
     console.log(`hello ${person.name}, you are ${person.age}, you are ${person.gender} :::`);
 }
 
 // What if I want to pass an object, 
 //we have to check wether the pbject type is right
-sayHi(person);
+sayHi(lynn);
 
 /* This file si going to be a module 
 * is we don't do this ...we cannot make variable*/
